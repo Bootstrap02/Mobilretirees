a611c4282726a697fce65fcd5550cb1b4e84eb42
Louis Ogochukwu Udegbue <143167253+Bootstrap02@users.noreply.github.com>
2026-06-12 07:22:20 +0100

    Update Firstpage.js

--- src/Pages/Firstpage.js
+++ src/Pages/Firstpage.js
@@ -8,7 +8,7 @@ import cacCertificate from '../assets/cac-certificate.jpg';
 import NotificationsList from '../Components/Notificationslist';
 import AlertModal from '../Components/Alerts';
 import axios from "axios";
-import Livechat from './FAQuestions/Livechat';
+import LiveChat from './FAQuestions/Livechat';
 
 const Homepage = () => {
   const navigate = useNavigate();
@@ -271,7 +271,7 @@ const Homepage = () => {
       <div>{notifications && <NotificationsList isOpen={openNotifications} onClose={closeNotifications} notifications={allNotifications}/>}</div>
       <div><AlertModal alert={alerts}/></div>
       <Footer />
-      <Livechat />          
+      <LiveChat />          
     </>
   );
 };
